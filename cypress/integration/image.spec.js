abc8ab4eaaba60c524a80ef4286922887b23d209
maclor <maciej.lorenc@gmail.com>
2020-10-24 21:13:28 +0200

    single element visual reggresion

--- cypress/integration/image.spec.js
+++ cypress/integration/image.spec.js
@@ -1,13 +1,25 @@
 const pages = [ 'http://example.com/' ]
 const sizes = [ 'iphone-6', 'ipad-2', [1024, 768]] 
 
-describe('Visual regression', () => {
+describe('Visual regression - whole page', () => {
     sizes.forEach(size => {
         pages.forEach( page => {
             it(`Should match ${page} in resolution ${size}`, () => {
                 cy.setResolution(size)
                 cy.visit(page)
                 cy.matchImageSnapshot()
+            })
+        })
+    })
+})
+
+describe('Visual regression - single element on page', () => {
+    sizes.forEach(size => {
+        pages.forEach( page => {
+            it(`Should match ${page} in resolution ${size}`, () => {
+                cy.setResolution(size)
+                cy.visit(page)
+                cy.get('h1').matchImageSnapshot()
             })
         })
     })
