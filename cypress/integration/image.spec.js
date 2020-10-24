48a876f93ec0c395f9111a08a4ab941807cd4c37
maclor <maciej.lorenc@gmail.com>
2020-10-24 20:47:16 +0200

    iterate over pages and sizes

--- cypress/integration/image.spec.js
+++ cypress/integration/image.spec.js
@@ -1,6 +1,14 @@
+const pages = [ 'http://example.com/' ]
+const sizes = [ 'iphone-6', 'ipad-2', [1024, 768]] 
+
 describe('Visual regression', () => {
-    it('First visual regression test', () =>{
-        cy.visit('http://example.com/')
-        cy.matchImageSnapshot()
+    sizes.forEach(size => {
+        pages.forEach( page => {
+            it(`Should match ${page} in resolution ${size}`, () => {
+                cy.setResolution(size)
+                cy.visit(page)
+                cy.matchImageSnapshot()
+            })
+        })
     })
 })
